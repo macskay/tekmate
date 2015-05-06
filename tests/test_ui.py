3713d205b99ee0a308684d20618e4a6c33a09292
Kevin Manire <kevin.manire@kaseya.com>
2015-05-06 15:02:35 -0700

    Fixed all UI unit tests.

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,13 +1,23 @@
 from unittest import TestCase
+from mock import Mock, patch
 import pygame
 
 from tekmate.ui import PlayerUI, ImageNotFound
 
 
 class PlayerUITestCase(TestCase):
+
     def setUp(self):
+        self.transform_patcher = patch("tekmate.ui.pygame.transform")
+        self.transform_patcher.start()
+        self.surface_patcher = patch("tekmate.ui.pygame.Surface")
+        self.surface = self.surface_patcher.start().return_value
         self.set_up_pygame_display()
         self.player_ui = PlayerUI()
+
+    def tearDown(self):
+        self.surface_patcher.stop()
+        self.transform_patcher.stop()
 
     def set_up_pygame_display(self):
         pygame.display.init()
@@ -18,3 +28,11 @@ class PlayerUITestCase(TestCase):
 
     def test_when_image_not_found_raise_error(self):
         self.assertRaises(ImageNotFound, self.player_ui.load_image, "player", "no_player.bmp")
+
+    def test_when_render_is_called_screen_should_get_cleared_and_image_is_drawn_to_surface(self):
+        mock_player_ui = Mock(PlayerUI)
+        mock_player_ui.surface = self.surface
+        mock_player_ui.render()
+
+        self.surface.fill.assert_called_with((250, 250, 250))
+        self.surface.blit.assert_called_with(mock_player_ui.image, (0, 0))
