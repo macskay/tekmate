570b5d0515ccf86e72cc642122dc65dd4f5e7622
Max <KlingmannM@gmail.com>
2015-05-06 05:31:08 +0200

    Branch rename and resolution change

--- tekmate/__main__.py
+++ tekmate/__main__.py
@@ -3,7 +3,7 @@ from tekmate.game import PyGameInitializer, TekmateFactory
 
 
 def main():
-    initializer = PyGameInitializer({"display_width": 1024, "display_height": 576})
+    initializer = PyGameInitializer({"display_width": 1280, "display_height": 800})
     game_factory = TekmateFactory(initializer)
     game = game_factory.create()
     game.enter_mainloop()
